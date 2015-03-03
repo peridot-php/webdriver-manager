b9f3ab8c967a00c2e7f0d582676552641fee0841
Brian Scaturro <scaturrob@gmail.com>
2015-03-03 12:39:09 -0500

    integration tests for os specifics

--- specs/integration/manager.it.php
+++ specs/integration/manager.it.php
@@ -3,7 +3,8 @@ use Peridot\WebDriverManager\Manager;
 
 describe('Manager', function () {
     beforeEach(function () {
-        $this->manager = new Manager();
+        $this->system = $this->getProphet()->prophesize('Peridot\WebDriverManager\OS\SystemInterface');
+        $this->manager = new Manager(null, null, $this->system->reveal());
 
         $selenium = glob($this->manager->getInstallPath() . '/selenium*');
         $chrome = glob($this->manager->getInstallPath() . '/chrome*');
@@ -16,13 +17,46 @@ describe('Manager', function () {
 
     describe('->update()', function () {
         context('when on a mac operating system', function () {
-            it('should download the latest selenium standalone and chrome driver', function () {
-                $this->manager->update();
-                $path = $this->manager->getInstallPath();
-                $selenium = glob("$path/selenium-server-standalone-*");
-                $chrome = glob("$path/chromedriver*");
-                expect($selenium)->to->have->length(1, 'no selenium file found');
-                expect($chrome)->to->have->length->at->least(2, 'no chrome files found');
+            beforeEach(function () {
+                $this->system->isMac()->willReturn(true);
+                $this->system->isWindows()->willReturn(false);
+                $this->system->isLinux()->willReturn(false);
+            });
+
+            require 'shared/manager-update.php';
+        });
+
+        context('when on a windows operating system', function () {
+            beforeEach(function () {
+                $this->system->isMac()->willReturn(false);
+                $this->system->isWindows()->willReturn(true);
+                $this->system->isLinux()->willReturn(false);
+            });
+
+            require 'shared/manager-update.php';
+        });
+
+        context('when on a linux operating system', function () {
+            beforeEach(function () {
+                $this->system->isMac()->willReturn(false);
+                $this->system->isWindows()->willReturn(false);
+                $this->system->isLinux()->willReturn(true);
+            });
+
+            context('and it is 32 bit linux', function () {
+                beforeEach(function () {
+                    $this->system->is64Bit()->willReturn(false);
+                });
+
+                require 'shared/manager-update.php';
+            });
+
+            context('and it is 64 bit linux', function () {
+                beforeEach(function () {
+                    $this->system->is64Bit()->willReturn(true);
+                });
+
+                require 'shared/manager-update.php';
             });
         });
     });
