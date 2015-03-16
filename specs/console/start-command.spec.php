85523da25786578651e52d672d6c522c7fcb5d1d
Brian Scaturro <scaturrob@gmail.com>
2015-03-16 15:27:34 -0400

    mock update command in start spec

--- specs/console/start-command.spec.php
+++ specs/console/start-command.spec.php
@@ -1,8 +1,5 @@
 <?php
-use Peridot\WebDriverManager\Binary\BinaryResolver;
-use Peridot\WebDriverManager\Binary\SeleniumStandalone;
 use Peridot\WebDriverManager\Console\StartCommand;
-use Peridot\WebDriverManager\Console\UpdateCommand;
 use Prophecy\Argument;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -11,20 +8,16 @@ describe('StartCommand', function () {
     beforeEach(function () {
         $this->application = new Application();
         $this->manager = $this->getProphet()->prophesize('Peridot\WebDriverManager\Manager');
+
+        $this->update = $this->getProphet()->prophesize('Peridot\WebDriverManager\Console\UpdateCommand');
+        $this->update->getName()->willReturn('update');
+        $this->update->setApplication($this->application)->shouldBeCalled();
+        $this->update->isEnabled()->willReturn(true);
+        $this->update->getDefinition()->willReturn($this->application->getDefinition());
+        $this->update->getAliases()->willReturn([]);
+
         $this->application->add(new StartCommand($this->manager->reveal()));
-        $this->application->add(new UpdateCommand($this->manager->reveal()));
-    });
-
-    beforeEach(function () { //mock the manager
-        $binaries = ['selenium' => new SeleniumStandalone(new BinaryResolver())];
-
-        $this->manager->getBinaries()->willReturn($binaries);
-        $this->manager->getPendingBinaries()->willReturn($binaries);
-        $this->manager->on('request.start', Argument::any())->shouldBeCalled();
-        $this->manager->on('progress', Argument::any())->shouldBeCalled();
-        $this->manager->on('complete', Argument::any())->shouldBeCalled();
-        $this->manager->update(Argument::any())->shouldBeCalled();
-        $this->manager->getInstallPath()->shouldBeCalled();
+        $this->application->add($this->update->reveal());
     });
 
     afterEach(function () {
@@ -33,7 +26,7 @@ describe('StartCommand', function () {
 
     describe('->execute()', function () {
         it('should update and start', function () {
-            $this->manager->update(Argument::any())->shouldBeCalled();
+            $this->update->run(Argument::any(), Argument::any())->shouldBeCalled();
             $this->manager->start(false, 4444)->shouldBeCalled();
 
             $command = $this->application->find('start');
@@ -44,7 +37,7 @@ describe('StartCommand', function () {
 
         context('when port is supplied', function () {
             it('should use the specified port if available', function () {
-                $this->manager->update(Argument::any())->shouldBeCalled();
+                $this->update->run(Argument::any(), Argument::any())->shouldBeCalled();
                 $this->manager->start(false, 9000)->shouldBeCalled();
 
                 $command = $this->application->find('start');
