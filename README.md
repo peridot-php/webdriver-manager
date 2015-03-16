54f6af20eaee425a547459a7af529feebc589f07
Brian Scaturro <scaturrob@gmail.com>
2015-03-16 11:48:33 -0400

    include documentation

--- README.md
+++
@@ -1,4 +0,0 @@
-#WebDriver Manager
-
-[![Build Status](https://travis-ci.org/peridot-php/webdriver-manager.png)](https://travis-ci.org/peridot-php/webdriver-manager)
-[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/peridot-php/webdriver-manager/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/peridot-php/webdriver-manager/?branch=master)
--- specs/integration/shared/manager-update.php
+++
@@ -1,9 +0,0 @@
-<?php
-it('should download the latest selenium standalone and chrome driver', function () {
-    $this->manager->update();
-    $path = $this->manager->getInstallPath();
-    $selenium = glob("$path/selenium-server-standalone-*");
-    $chrome = glob("$path/chromedriver*");
-    expect($selenium)->to->have->length(1, 'no selenium file found');
-    expect($chrome)->to->have->length->at->least(2, 'no chrome files found');
-});
